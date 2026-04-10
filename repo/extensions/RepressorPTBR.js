a86d46fa6fbb68034a870fe495e8615324251c38
Alezerker <alexandreaparecido50@hotmail.com>
2026-04-10 16:41:03 -0300

    Create RepressorPTBR.js

---
+++ repo/extensions/RepressorPTBR.js
@@ -0,0 +1,21 @@
+function getInfo() {
+  return {
+    name: "Como Usar um Regressor",
+    id: "regressor-ptbr",
+    lang: "pt-BR",
+    baseUrl: "https://neoxscans.net"
+  }
+}
+
+async function getChapters() {
+  return [
+    {
+      name: "Capítulo 1",
+      url: "/manga/como-usar-um-regressor/"
+    }
+  ]
+}
+
+async function getPages() {
+  return []
+}
